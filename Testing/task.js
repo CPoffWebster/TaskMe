2f3d7fa5e9b80b5ffa3fdfa77b3244236b1f67fc
Duncan Calder <DCalder@clarku.edu>
2019-03-11 15:05:12 -0400

    addding comments v2

--- Testing/task.js
+++ Testing/task.js
@@ -1,16 +1,25 @@
+//array to hold task objects
 var tasks = [];
+    //function to get information about task from user
    function getValue() {
       var taskName = prompt("Enter your task name : ", "your task name here");
       var taskDate = prompt("Enter your due date : ", "your due date here");
+      //make new tasks with name, date and default weight, 1
       var newTask = new task(taskName, taskDate, 1);
+      //push new task onto array
       tasks.push(newTask);
+      //call method to sort and print tasks to console
       printSort(tasks);
    }
+
+//task object
 function task(name, due, weight) {
             this.name = name;
             this.due  = due;
             this.weight = weight;
          }
+//sort array by different attributes
+//compareType is 1 for sorting by name, 2 for due date, 3 for weight, and default is name
 function sortTasks(array, compareType) {
     this.array = array;
     switch (compareType) {
@@ -26,6 +35,7 @@ function sortTasks(array, compareType) {
         default: array.sort(function(a, b){return a.name.localeCompare(b.name)});
     }
 }
+//sorts tasks and prints them to console
 function printSort(array) {
     this.array = array;
     sortTasks(array, 2);

ed82ef6ce13e4affa9ce85d24e05ed5e3a2c5f80
Trevor Erwin <trevorman1024@gmail.com>
2019-03-11 15:14:42 -0400

    Full UI prototype v1

---
+++ task-manager/app.js
@@ -0,0 +1,20 @@
+$(function () {
+
+    // page is now ready, initialize the calendar...
+
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: '.',
+            right: 'title',
+        },
+        defaultView: 'agendaWeek',
+        contentHeight: 600,
+        allDaySlot: false,
+        selectable: true,
+        themeSystem: 'jquery-ui',
+        minTime: "07:00:00",
+        maxTime: "22:00:00"
+    })
+
+});

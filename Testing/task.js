56290e117b2c232d5ba3a99f4f1c7fa1a3550a27
CPOFFWEBSTER <cpoffwebster@clarku.edu>
2019-03-11 21:49:36 -0400

    Updated Parameters

--- Testing/task.js
+++ Testing/task.js
@@ -2,21 +2,26 @@
 var tasks = [];
     //function to get information about task from user
    function getValue() {
-      var taskName = prompt("Enter your task name : ", "your task name here");
-      var taskDate = prompt("Enter your due date : ", "your due date here");
+      var name = document.getElementById("TaskName").value;
+      var dueDate = document.getElementById("DueDate").value;
+      var difficulty = document.getElementById("Difficulty").value;
+      var hours = document.getElementById("Hours").value;
+      var minutes = document.getElementById("Minutes").value;
       //make new tasks with name, date and default weight, 1
-      var newTask = new task(taskName, taskDate, 1);
+      var newTask = new task(name, dueDate, difficulty, hours, minutes);
       //push new task onto array
       tasks.push(newTask);
       //call method to sort and print tasks to console
       printSort(tasks);
    }
-// HELLO THIS IS A RANDOM TEST INPUT
+
 //task object
-function task(name, due, weight) {
+function task(name, due, difficulty, hours, minutes) {
             this.name = name;
             this.due  = due;
-            this.weight = weight;
+            this.difficulty = difficulty;
+            this.hours = hours;
+            this.minutes = minutes;
          }
 //sort array by different attributes
 //compareType is 1 for sorting by name, 2 for due date, 3 for weight, and default is name
@@ -29,7 +34,7 @@ function sortTasks(array, compareType) {
         case 2: array.sort(function(a, b){return b.due - a.due});
         break;
 
-        case 3: array.sort(function(a, b){return b.weight - a.weight});
+        case 3: array.sort(function(a, b){return b.difficulty - a.difficulty});
         break;
 
         default: array.sort(function(a, b){return a.name.localeCompare(b.name)});

c1bc38535a4e45a917f1381675db1bf1946673d2
CPOFFWEBSTER <cpoffwebster@clarku.edu>
2019-03-15 14:23:57 -0400

    Add Task Button

--- task-manager/app2.js
+++ task-manager/app2.js
@@ -2,14 +2,28 @@
 let modal = document.querySelector('.modal');
 
 // Get the button that opens the modal
-let btn = document.querySelector(".add-task");
+let openBtn = document.querySelector(".add-task");
 
 // Get the <span> element that closes the modal
 let span = document.querySelector(".close")
 
+// Get the button that closes the modal (done AND delete)
+let doneBtn = document.querySelector(".doneBtn");
+
+// Get the button that closes the modal (done AND delete)
+let deleteBtn = document.querySelector(".deleteBtn");
+
 // When the user clicks the button, open the modal
-btn.addEventListener("click", function () {
+openBtn.addEventListener("click", function () {
     modal.style.display = "block";
+})
+
+doneBtn.addEventListener("click", function () {
+    modal.style.display = 'none';
+})
+
+deleteBtn.addEventListener("click", function () {
+    modal.style.display = 'none';
 })
 
 // When the user clicks on <span> (x), close the modal
@@ -22,4 +36,12 @@ window.addEventListener('click', function (e) {
     if (e.target == modal) {
         //modal.style.display = 'none';
     }
+
+// When the presses ESCAPE, close the modal
+window.onkeydown = function(evt) {
+    evt = evt || window.event;
+    if (evt.keyCode == 27) {
+        modal.style.display = 'none';
+    }
+};
 })
---
+++ task-manager/task.js
@@ -0,0 +1,87 @@
+
+
+//array to hold task objects
+let tasks = [];
+//function to get information about task from user
+function getValue() {
+  let name = document.getElementById("taskName").value;
+  let dueDate = new Date(document.getElementById("dueDate").value);
+  let urgency = (document.getElementById("urgency").value) / 10;
+  let hours = document.querySelector(".hours").value;
+  let minutes = document.querySelector(".minutes").value;
+
+  if (isNaN(Date.parse(dueDate))) {
+      throw "Invalid Date, needs to be in format: mm/dd/yyyy";
+  }
+  //make new tasks with name, date, urgency, and hours/minutes to completion
+  let newTask = new Task(name, dueDate, urgency, hours, minutes);
+  //push new task onto array
+  tasks.push(newTask);
+  //call method to sort and print tasks to console
+  printSort(tasks, 2);
+  clearValues();
+}
+
+function clearValues() {
+    document.getElementById("taskName").value = "";
+    document.getElementById("dueDate").value = "";
+    document.getElementById("urgency").value = "";
+    document.querySelector(".hours").value = "";
+    document.querySelector(".minutes").value = "";
+}
+
+//task object
+function Task(name, due, urgency, hours, minutes) {
+  this.name = name;
+  this.due = due;
+  this.urgency = urgency;
+  this.hours = hours;
+  this.minutes = minutes;
+}
+//sort array by different attributes
+//compareType is 1 for sorting by name, 2 for due date, 3 for weight, and default is name
+function sortTasks(array, compareType) {
+  this.array = array;
+  switch (compareType) {
+      case 1:
+      array.sort(function(a, b) {
+          return a.name.localeCompare(b.name);
+      });
+      break;
+
+      case 2:
+      array.sort(function(a, b) {
+          return b.due.getTime() > a.due.getTime();
+      });
+      break;
+
+      case 3:
+      array.sort(function(a, b) {
+          return b.urgency - a.urgency;
+      });
+      break;
+
+      case 4:
+      array.sort(function(a, b) {
+          if (b.hours - a.hours != 0) {
+              return b.hours - a.hours;
+          }
+          else {
+              return b.minutes - a.hours;
+          }
+
+      });
+      break;
+
+    default:
+      array.sort(function(a, b) {
+        return a.name.localeCompare(b.name);
+      });
+  }
+}
+//sorts tasks and prints them to console
+function printSort(array, sortType) {
+  this.array = array;
+  sortTasks(array, sortType);
+  console.log(array);
+}

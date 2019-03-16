cea5be43a3e2cbc1e69d1a5c418c85b191bad807
Trevor Erwin <trevorman1024@gmail.com>
2019-03-16 17:18:20 -0400

    Added month view, option to drag events and change duration

--- task-manager/app.js
+++ task-manager/app.js
@@ -5,16 +5,28 @@ $(function () {
     $('#calendar').fullCalendar({
         header: {
             left: 'prev,next today',
-            center: '.',
-            right: 'title',
+            center: 'title',
+            right: 'month,agendaWeek',
         },
         defaultView: 'agendaWeek',
         contentHeight: 500,
         allDaySlot: false,
         selectable: true,
+        selectHelper: true,
+        eventStartEditable: true,
+        eventDurationEditable: true,
         themeSystem: 'jquery-ui',
         minTime: "07:00:00",
-        maxTime: "22:00:00"
+        maxTime: "22:00:00",
+
+        events: [
+            {
+              title  : 'event test',
+              start  : '2019-03-15T13:30:00',
+              end    : '2019-03-15T16:30:00',
+            },
+        ],
+
     })
 
 });
